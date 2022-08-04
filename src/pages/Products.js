852c797ada6ad939e0880c8400153b29e27b6877
imkarthi16 <karthiveera@gmail.com>
2022-08-04 13:17:40 +0530

    Using Switch and exact for configuring routes

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,16 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Products = () => {
   return (
     <section>
       <h1>The Product Details</h1>
       <ul>
-        <li>A Book</li>
-        <li>A Carpet</li>
-        <li>An Online course</li>
+        <li>
+          <Link to="/products/p1">A Book</Link>
+        </li>
+        <li>
+          <Link to="/products/p2">A Carpet</Link>
+        </li>
+        <li>
+          <Link to="/products/p3">An Online course</Link>
+        </li>
       </ul>
     </section>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
