6cfcce20c954a413dc808f8fe065d34467d79c9e
imkarthi16 <karthiveera@gmail.com>
2022-08-08 13:52:41 +0530

    Flexible Routing Code using useRouteMatch hook

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,27 +1,36 @@
 import { Fragment } from 'react';
-import { Route, useParams } from 'react-router-dom';
-import Comments from '../components/comments/Comments';
+import { useParams, Route, Link, useRouteMatch } from 'react-router-dom';
+
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import Comments from '../components/comments/Comments';
 
 const DUMMY_QUOTES = [
-  {id:'q1', author:'Max', text:'Learning React is fun!'},
-  {id:'q2', author:'Maximilian', text:'Learning React is Great!'}
+  { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
+  { id: 'q2', author: 'Maximilian', text: 'Learning React is great!' },
 ];
 
 const QuoteDetail = () => {
+  const match = useRouteMatch();
   const params = useParams();
 
-  const quote = DUMMY_QUOTES.find((quote)=>quote.id === params.quoteId)
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
-  if(!quote){
-    return <p>No quote found!</p>
+  if (!quote) {
+    return <p>No quote found!</p>;
   }
 
   return (
     <Fragment>
-      <HighlightedQuote text={quote.text} author={quote.author}/>
-      <Route path={`/quotes/${params.quoteId}/comments`}>
-        <Comments/>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={match.path} exact>
+        <div className='centered'>
+          <Link className='btn--flat' to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`${match.path}/comments`}>
+        <Comments />
       </Route>
     </Fragment>
   );
